2527840dd03a3b54c4ca48bcd721dd9ca1c0f6e5
George <georgemwazuna3@gmail.com>
2025-04-12 20:47:50 +0300

    More tweaks to the CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
+import "./index.css";
 
 const dummyExpenses = [
   { id: 1, title: "Groceries", amount: 50, date: "2025-04-01" },
